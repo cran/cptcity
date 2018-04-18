7f5a7329d940b7c2425455c861b6f4a802400d83
Sergio Ibarra-Espinosa <sergio.ibarra@usp.br>
2018-04-18 15:29:47 +0000

    version 1.0.3

--- R/cpt.R
+++ R/cpt.R
@@ -1,16 +1,17 @@
-#' Function to return colour palletes functions from 'cpt-city'
+#' Function to return colour palettes functions from 'cpt-city'
 #'
-#' This function return a collor pallete based on the name or position of the
-#' pallete.
+#' This function return a collor palette based on the name or position of the
+#' palette.
 #'
 #' The cpt-city web archive comes from:
 #' http://soliton.vm.bytemark.co.uk/pub/cpt-city/index.html
 #'
-#' @param pal Pallete of colors available or the number of the position
+#' @param pal Palette of colors available or the number of the position
 #' @param n integer; number of colors
-#' of the same
+#' @param colorRampPalette Logical; to be used in sf and mapview.
 #'
-#' @return A colour pallete function.
+#'
+#' @return A colour palette function.
 #' @importFrom grDevices rgb colorRampPalette
 #' @export
 #' @examples {
@@ -29,12 +30,16 @@
 #'  scale_fill_gradientn(colours = cpt(n = 100))
 #' }
 #' }
-cpt <- function(pal = "mpl_inferno", n = 100){
+cpt <- function(pal = "mpl_inferno", n = 100, colorRampPalette = FALSE){
   m <- sysdata[[pal]]
   col <-  rgb(red = m$r, green = m$g, blue = m$b,
               maxColorValue = max(c(max(m$r, na.rm = T),
                                     max(m$g, na.rm = T),
                                     max(m$b, na.rm = T)))
               )
-  grDevices::colorRampPalette(col)(n)
+  if(colorRampPalette == FALSE){
+    return(grDevices::colorRampPalette(col)(n))
+  } else{
+    return(grDevices::colorRampPalette(col))
+  }
 }
---
+++ R/lucky.R
@@ -0,0 +1,51 @@
+#' Random colour gradient!
+#'
+#' Based on "I'm Feeling Lucky" from Google. As this package includes 7140
+#' colour gradients, it might be hard to find the 'right'
+#'
+#' The cpt-city web archive comes from:
+#' http://soliton.vm.bytemark.co.uk/pub/cpt-city/index.html
+#'
+#' @param n integer; number of colors
+#' @param message Logical; for printing or not the name of the colour gradient
+#' @param nseed integer; for reproducing the same colour gradient.
+#' See \code{\link{set.seed}}
+#' @param colorRampPalette Logical; to be used in sf and mapview.
+#'
+#' @return A RANDOM colour palette function including name of the colour gradient
+#' and number.
+#' @importFrom grDevices rgb colorRampPalette
+#' @importFrom stats runif
+#' @export
+#' @examples {
+#' library(cptcity)
+#' image(matrix(1:100), col = lucky())
+#' image(matrix(1:100), col = lucky())
+#' image(matrix(1:100), col = lucky())
+#' image(matrix(1:100), col = lucky())
+#' image(matrix(1:100), col = lucky())
+#' image(matrix(1:100), col = lucky(nseed = 1))
+#' }
+lucky <- function(n = 100, colorRampPalette = FALSE, message = TRUE, nseed){
+  if(!missing(nseed)){
+    set.seed(nseed)
+    numero <- round(runif(n = 1L, min = 0, max = 7140))
+  } else {
+    numero <- round(runif(n = 1L, min = 0, max = 7140))
+  }
+  m <- sysdata[[numero]]
+  cpt_names <- sysdata[["cpt_names"]]
+  col <-  rgb(red = m$r, green = m$g, blue = m$b,
+              maxColorValue = max(c(max(m$r, na.rm = T),
+                                    max(m$g, na.rm = T),
+                                    max(m$b, na.rm = T)))
+  )
+  if(message){
+    cat(paste0("Colour gradient: " ,cpt_names[numero], ", number: ", numero))
+  }
+  if(colorRampPalette == FALSE){
+    return(grDevices::colorRampPalette(col)(n))
+  } else{
+    return(grDevices::colorRampPalette(col))
+  }
+}
